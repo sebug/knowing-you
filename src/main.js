d85aa574433faf15f86c46dd2ecf2e309e012538
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-04-03 16:01:43 +0000

    Generic message trigger

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('Keep waiting');
+async function getMessage() {
+    const genericMessageResponse = await fetch('/api/GenericMessageTrigger');
+    const genericContent = await genericMessageResponse.text();
+
+    const messagePlaceholder = document.querySelector('#message-placeholder');
+    if (messagePlaceholder) {
+        messagePlaceholder.innerHTML = genericContent;
+    }
+}
+
+getMessage();
