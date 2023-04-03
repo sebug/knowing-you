ff8b9ee6453555891bfa97263aebd6948a2067c4
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-04-03 17:00:22 +0000

    Register - get challenge

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+async function register() {
+    const challengeResponse = await fetch('/api/CreateChallengeTrigger');
+    const challenge = await challengeResponse.json();
+
+    console.log(challenge);
+}
+
+
 async function getMessage() {
     const genericMessageResponse = await fetch('/api/GenericMessageTrigger');
     const genericContent = await genericMessageResponse.text();
@@ -9,3 +17,5 @@ async function getMessage() {
 }
 
 getMessage();
+
+register();
