c1129a063716c44971f5714ebd4dc603c528866d
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-04-04 12:00:13 +0000

    Responding with error message

--- api/RegisterTrigger/index.js
+++ api/RegisterTrigger/index.js
@@ -1,23 +1,30 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    try {
+        context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (!req.body || !req.body.id || !req.body.clientDataJSON || !req.body.attestationObject) {
+        if (!req.body || !req.body.id || !req.body.clientDataJSON || !req.body.attestationObject) {
+            context.res = {
+                status: 400,
+                body: "Malformed object"
+            };
+        }
+    
+        const clientData = JSON.parse(new TextDecoder().decode(atob(req.body.clientDataJSON)));
+    
+        const response = {
+            id: req.body.id,
+            clientData: clientData,
+            attestationObject: req.body.attestationObject
+        };
+    
         context.res = {
-            status: 400,
-            body: "Malformed object"
+            // status: 200, /* Defaults to 200 */
+            body: response
+        };
+    } catch (e) {
+        context.res = {
+            status: 500,
+            body: '' + e
         };
     }
-
-    const clientData = JSON.parse(new TextDecoder().decode(atob(req.body.clientDataJSON)));
-
-    const response = {
-        id: req.body.id,
-        clientData: clientData,
-        attestationObject: req.body.attestationObject
-    };
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: response
-    };
 }
